1a2162adc59bcdd1a09cf1ff6d9a162cac5a1335
Sadman Sakib Jisan <65308095+JisanEAE77@users.noreply.github.com>
2023-11-18 04:28:08 +0600

    minor change in  ToDo app variable list value

--- rlf/src/App.jsx
+++ rlf/src/App.jsx
@@ -2,7 +2,16 @@ import "./App.css";
 import ToDo from "./TODOAPP/ToDo";
 
 const App = () => {
-  const todolist = null
+  const todolist = [
+    {
+      task: "Meeting with a client",
+      date: "20/11/2023",
+    },
+    {
+      task: "Need to get a hair cut",
+      date: "23/11/2023",
+    },
+  ];
 
   return (
     <>
