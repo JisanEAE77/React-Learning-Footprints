9cbef57972eed3a8e07b0194d68f60741e2dc4ca
Sadman Sakib Jisan <65308095+JisanEAE77@users.noreply.github.com>
2023-11-17 15:31:07 +0600

    Created First React Component
    Custom Button Component with its own CSS

---
+++ rlf/src/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css'
+
+const Button = () => {
+    return (
+        <>
+            <button className="button">This is a custom Button Component</button>
+        </>
+    )
+}
+
+export default Button;
